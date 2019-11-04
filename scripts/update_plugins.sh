2f74c8fbc2c0c056e7ac67158cb61966e1aa9ea3
timbeccue <timothybeccue@gmail.com>
2019-11-04 15:29:23 -0800

    Fix critical bug in shell script that would corrupt the OS :)

--- scripts/update_plugins.sh
+++ scripts/update_plugins.sh
@@ -3,8 +3,8 @@
 SCRIPTS_DIR="/home/ubuntu/scripts"
 PREV_SCRIPTS_DIR="/home/ubuntu/prev_scripts"
 TEMP_SCRIPTS_DIR="/home/ubuntu/temp_scripts"
-ANLAYSIS_WRAPPERS_DIR="/var/www/js9/analysis-wrappers"
-ANLAYSIS_PLUGINS_DIR="/var/www/js9/analysis-plugins"
+ANALYSIS_WRAPPERS_DIR="/var/www/js9/analysis-wrappers"
+ANALYSIS_PLUGINS_DIR="/var/www/js9/analysis-plugins"
 
 mkdir -p /home/ubuntu/prev_scripts/analysis-wrappers
 mkdir -p /home/ubuntu/prev_scripts/analysis-plugins
@@ -16,15 +16,15 @@ chown -R ubuntu:ubuntu /home/ubuntu/
 git clone https://github.com/lcogt/ptr_js9 $TEMP_SCRIPTS_DIR
 
 # Move old content to archive-like directory
-mv $ANALYSIS_WRAPPERS_DIR/* 			/home/ubuntu/prev_scripts/analysis-wrappers
-mv $ANALYSIS_PLUGINS_DIR/* 			/home/ubuntu/prev_scripts/analysis-plugins
-mv $SCRIPTS_DIR/*.py 				/home/ubuntu/prev_scripts/python
+mv $ANALYSIS_WRAPPERS_DIR/* /home/ubuntu/prev_scripts/analysis-wrappers
+mv $ANALYSIS_PLUGINS_DIR/* /home/ubuntu/prev_scripts/analysis-plugins
+mv $SCRIPTS_DIR/*.py /home/ubuntu/prev_scripts/python
 
 # Move new content into place
-mv $TEMP_SCRIPTS_DIR/analysis-wrappers/* 	$ANALYSIS_WRAPPERS_DIR/
-mv $TEMP_SCRIPTS_DIR/analysis-plugins/* 	$ANALYSIS_PLUGINS_DIR/
-mv $TEMP_SCRIPTS_DIR/python/*.py 		$SCRIPTS_DIR/
-mv $TEMP_SCRIPTS_DIR/python/requirements.txt	$SCRIPTS_DIR/
+mv $TEMP_SCRIPTS_DIR/analysis-wrappers/* $ANALYSIS_WRAPPERS_DIR/
+mv $TEMP_SCRIPTS_DIR/analysis-plugins/* $ANALYSIS_PLUGINS_DIR/
+mv $TEMP_SCRIPTS_DIR/python/*.py $SCRIPTS_DIR/
+mv $TEMP_SCRIPTS_DIR/python/requirements.txt $SCRIPTS_DIR/
 
 # Restart node helper: 
 su - ubuntu -c "kill -USR2 `ps guwax | egrep js9Helper | egrep -v egrep | awk '{print $2}'`"
