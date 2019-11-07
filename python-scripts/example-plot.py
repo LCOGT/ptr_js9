fcd897b2108afc2a8cb3871caca5b178cb8b6c95
timbeccue <timothybeccue@gmail.com>
2019-11-07 14:06:03 -0800

    Add example plot script

---
+++ python-scripts/example-plot.py
@@ -0,0 +1,16 @@
+import sys, json, random
+
+data_to_plot = []
+
+for column in range(20):
+    value = random.randint(0,10)
+    data_to_plot.append([column,value])
+
+properly_formatted_output = json.dumps({
+    "color": "red",
+    "label": "random.randint(0,10) vs. column",
+    "data": data_to_plot,
+})
+
+print(properly_formatted_output)
+sys.exit(100)
