6e6a38fa8026555912ff10e2847de8a6623febe2
timbeccue <timothybeccue@gmail.com>
2019-11-07 11:47:42 -0800

    Bugfix with restarting node helper

--- server-management/update-scripts.sh
+++ server-management/update-scripts.sh
@@ -4,10 +4,10 @@ cd /home/ubuntu/ptr_js9
 git pull
 
 # Restart node helper: 
-su - ubuntu -c "kill -USR2 `ps guwax | egrep js9Helper | egrep -v egrep | awk '{print $2}'`"
+su - ubuntu -c "kill -USR2 `ps guwax | egrep js9Helper | egrep -v grep | awk '{print $2}'`"
 
 # Update python requirements in our virtual environment
-su -ubuntu -c "source /home/ubuntu/ptr_js9/python-scripts/venv/bin/activate 
+su - ubuntu -c "source /home/ubuntu/ptr_js9/python-scripts/venv/bin/activate 
 pip3 install -r /home/ubuntu/ptr_js9/python-scripts/requirements.txt
 deactivate"
 
