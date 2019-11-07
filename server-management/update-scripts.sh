f3649298e08f3e7ffae763f9b8b3d5256be2edd5
timbeccue <timothybeccue@gmail.com>
2019-11-07 09:50:09 -0800

    bugfixes

--- server-management/update-scripts.sh
+++ server-management/update-scripts.sh
@@ -7,13 +7,9 @@ git pull
 su - ubuntu -c "kill -USR2 `ps guwax | egrep js9Helper | egrep -v egrep | awk '{print $2}'`"
 
 # Update python requirements in our virtual environment
-update_python=$(cat << END
-update_python='source /home/ubuntu/ptr_js9/python-scripts/venv/bin/activate
+su -ubuntu -c "source /home/ubuntu/ptr_js9/python-scripts/venv/bin/activate 
 pip3 install -r /home/ubuntu/ptr_js9/python-scripts/requirements.txt
-deactivate
-END
-)
-su - ubuntu -c $update_python
+deactivate"
 
 #### OLD
 
